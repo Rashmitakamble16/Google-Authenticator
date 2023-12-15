37be5786d0fe1472998ee7b236a77bc00e81df32
RASHMITA RAJENDRA KAMBLE <119934172+Rashmitakamble16@users.noreply.github.com>
2023-12-15 17:45:05 +0530

    Program.cs

--- TokenAuthNet_Initial-main/AuthenticationApi/Program.cs
+++ TokenAuthNet_Initial-main/AuthenticationApi/Program.cs
@@ -10,35 +10,14 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("LaunchSettings"));
-
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie();
-//services.AddAuthentication()
-//    .AddGoogleAuthenticator("GoogleAuthenticator", options =>
-//    {
-//        options.Secret = "YourSecretKey";
-//        options.QrCodeUri = "YourQRCodeUri";
-//    });
-
-
-
-//builder.Services.AddAuthentication(options =>
-//     {
-//         options.DefaultAuthenticateScheme = Google.;
-//         options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-//     })
-//            .AddGoogle(options =>
-//            {
-//                options.ClientId = "[MyGoogleClientId]";
-//                options.ClientSecret = "[MyGoogleSecretKey]";
-//            });
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
 
 var app = builder.Build();
 
@@ -56,4 +35,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
